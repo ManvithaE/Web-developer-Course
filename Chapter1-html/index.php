b2a20563a17370893be8f18c45323eb148df758e
Manvitha <26777125+ManvithaE@users.noreply.github.com>
2017-11-02 08:31:27 +1300

    Initial commit

---
+++ Chapter1-html/index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+    <head>
+        <title> Home Page</title>
+    </head>
+    <body>
+        <h1>About Cricket</h1>
+        <p>Cricket is a national sport which is played between two teams of eleven players each who score runs (points) by running between two sets of three small, wooden posts called wickets. Each of the wickets is at one end of a rectangle of flattened grass called the pitch</p>
+        <img src ="cricket.jpg" width="400"/>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/5XiBpO7i1do" frameborder="0" allowfullscreen align="right">
+        </iframe> 
+        <h3>About</h3>
+        <p>There are two teams: </p>
+        <ul>
+            <li>The team bowling has 11 players on the field.</li>
+            <li>The team batting always has two players on the field.</li>
+            <li>The captain of the bowling team chooses a bowler from his team; the other 10 players are called fielders. The bowler is trying to aim the ball at a <a href="https://simple.wikipedia.org/wiki/Wicket" >wicket</a></li>
+        </ul>
+        <hr>
+        <h3> All about Cricket</h3>
+        <ol>
+            <li>The team bowling has 11 players on the field.</li>
+            <li>The team batting always has two players on the field.</li>
+            <li>The captain of the bowling team chooses a bowler from his team; the other 10 players are called fielders. The bowler is trying to aim the ball at a <a href="https://simple.wikipedia.org/wiki/Wicket" >wicket</a></li>
+        </ol>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Country</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Sachin</td>
+                    <td>India</td>
+                </tr>
+                <tr>
+                    <td>Sangakara</td>
+                    <td>Srilanka</td>
+                </tr>
+            </tbody>
+        </table>
+        <form action="https://www.google.com">
+            <p>Want to know more</p>
+            <p><input type="submit"/></p>
+        </form>
+           </body>
+</html>
